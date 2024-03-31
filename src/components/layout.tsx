e657b888aca9e6fbacfa069ec0196932a84490ca
Felipe da Silva <silvaafelipe016@gmail.com>
2024-03-31 11:48:17 -0300

    feat: added framer-motion

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,6 +2,8 @@ import React, { ReactNode } from "react";
 
 import styled from "styled-components";
 
+import { motion } from "framer-motion";
+
 import "@/utils/i18n";
 
 import { Navbar } from "./navbar";
@@ -20,7 +22,17 @@ export const Layout = ({ children }: ILayoutProps) => {
   return (
     <LayoutContainer>
       <Navbar />
-      {children}
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        {children}
+      </motion.div>
       <Footer />
     </LayoutContainer>
   );
